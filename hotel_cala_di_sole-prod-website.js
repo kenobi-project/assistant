30fd2f31e2888a052a8b93454f8c532f316b3140
no-name <contact@muvia-solutions.fr>
2026-03-25 14:04:51 +0100

    Create hotel_cala_di_sole-prod-website.js

---
+++ hotel_cala_di_sole-prod-website.js
@@ -0,0 +1,107 @@
+(function() {
+    // Fonction principale
+    /* function initBotpress() {
+        
+        // --- 🔒 SÉCURITÉ : ALLOW ORIGIN ---
+        const currentDomain = window.location.hostname;
+        const allowedDomains = ["www.caladisole.fr", "caladisole.fr"];
+
+        // Si le domaine n'est pas dans la liste, on arrête tout.
+        if (!allowedDomains.includes(currentDomain)) {
+            console.warn("⛔ MUVIA Assitant : Licence non valide pour ce domaine (" + currentDomain + ").");
+            return; // Arrêt immédiat du script
+        }
+        // ------------------------------------
+*/
+        // 1. Ajouter le script Botpress
+        const botpressScript = document.createElement('script');
+        botpressScript.src = "https://cdn.botpress.cloud/webchat/v3.5/inject.js";
+        document.head.appendChild(botpressScript);
+
+        // 2. Ajouter le style
+        const style = document.createElement('style');
+        style.textContent = `
+          #webchat .bpWebchat {
+            position: unset;
+            width: 100%;
+            height: 100%;
+            max-height: 100%;
+            max-width: 100%;
+          }
+          #webchat .bpFab {
+            display: none;
+          }
+        `;
+        document.head.appendChild(style);
+
+        // 3. Créer le conteneur du webchat
+        if (!document.getElementById('webchat') && document.body) {
+            const webchatDiv = document.createElement('div');
+            webchatDiv.id = 'webchat';
+            webchatDiv.style.width = '500px';
+            webchatDiv.style.height = '500px';
+            // Z-index très élevé pour être sûr qu'il passe devant tout le reste du site
+            webchatDiv.style.zIndex = '2147483647'; 
+            document.body.appendChild(webchatDiv);
+        }
+
+        // 4. Initialiser Botpress
+        botpressScript.onload = () => {
+            window.botpress.on("webchat:ready", () => {
+                window.botpress.open();
+            });
+
+            window.botpress.init({
+                botId: "da219794-2bd8-48c7-b161-f5045730b8be",
+                configuration: {
+                    version: "v2",
+                    composerPlaceholder: "Posez votre question...",
+                    botName: "Cala di Sole ★★★",
+                    botAvatar: "https://files.bpcontent.cloud/2026/03/25/12/20260325125005-Q3BKYI9Y.jpeg",
+                    botDescription: "Situé dans une des plus belles criques du Golfe d'Ajaccio, l'hôtel Cala di Sole vous accueillera pour votre séjour en Corse dans une de ses nombreuses chambres, toute vue direct sur la mer.",
+                    fabImage: "https://files.bpcontent.cloud/2026/03/25/12/20260325125205-DQ6QXC83.jpeg",
+                    website: { title: "Site Web", link: "https://www.caladisole.fr/" },
+                    email: { title: "hotelcaladisole@gmail.com", link: "hotelcaladisole@gmail.com" },
+                    phone: { title: "+33495520136", link: "+33495520136" },
+                    termsOfService: {},
+                    privacyPolicy: {},
+                    color: "#02a5ad",
+                    variant: "solid",
+                    additionalStylesheetUrl: "https://kenobi-project.github.io/assistant/hotel_cala_di_sole-webapp_style.css",
+                    headerVariant: "glass",
+                    themeMode: "light",
+                    fontFamily: "Inter",
+                    radius: 1.1,
+                    feedbackEnabled: false,
+                    footer: "[Développé par MUVIA. 🚀](https://muvia-solutions.fr/)",
+                    storageLocation: "localStorage",
+                    soundEnabled: true,
+                    proactiveMessageEnabled: true,
+                    proactiveBubbleMessage: "Besoin d'aide ?👋",
+                    proactiveBubbleTriggerType: "afterDelay",
+                    proactiveBubbleDelayTime: 5
+                },
+                clientId: "a4cc1cd3-ff80-4e95-835b-46dfdd4cae28",
+                selector: "#webchat"
+            });
+
+             window.botpress.on("webchat:ready", () => {
+    // On vérifie dans le stockage local si le bot a déjà été déclenché
+    const hasTriggeredBefore = localStorage.getItem("botpress_permanent_trigger");
+    if (!hasTriggeredBefore) {
+      window.botpress.sendEvent({type:"siteweb"});
+      // On enregistre l'information de manière permanente
+      localStorage.setItem("botpress_permanent_trigger", "true");
+    }
+  });
+            
+        };
+    }
+
+    // Lancement intelligent (Dès que possible)
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        initBotpress();
+    } else {
+        document.addEventListener("DOMContentLoaded", initBotpress);
+    }
+})();
