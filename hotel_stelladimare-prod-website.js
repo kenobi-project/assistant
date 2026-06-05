1231199563c60ef0bd5057413b0e2a5b052c882f
no-name <contact@muvia-solutions.fr>
2026-06-05 16:05:25 +0200

    Create hotel_stelladimare-prod-website.js

---
+++ hotel_stelladimare-prod-website.js
@@ -0,0 +1,107 @@
+(function() {
+    // Fonction principale
+     function initBotpress() {
+        /*
+        // --- 🔒 SÉCURITÉ : ALLOW ORIGIN ---
+        const currentDomain = window.location.hostname;
+        const allowedDomains = ["www.caladisole.fr", "caladisole.fr"];
+
+        // Si le domaine n'est pas dans la liste, on arrête tout.
+        if (!allowedDomains.includes(currentDomain)) {
+            console.warn("⛔ MUVIA Assitant : Licence non valide pour ce domaine (" + currentDomain + ").");
+            return; // Arrêt immédiat du script
+        }
+        // ------------------------------------
+*/
+        // 1. Ajouter le script Botpress
+        const botpressScript = document.createElement('script');
+        botpressScript.src = "https://cdn.botpress.cloud/webchat/v3.5/inject.js";
+        document.head.appendChild(botpressScript);
+
+        // 2. Ajouter le style
+        const style = document.createElement('style');
+        style.textContent = `
+          #webchat .bpWebchat {
+            position: unset;
+            width: 100%;
+            height: 100%;
+            max-height: 100%;
+            max-width: 100%;
+          }
+          #webchat .bpFab {
+            display: none;
+          }
+        `;
+        document.head.appendChild(style);
+
+        // 3. Créer le conteneur du webchat
+        if (!document.getElementById('webchat') && document.body) {
+            const webchatDiv = document.createElement('div');
+            webchatDiv.id = 'webchat';
+            webchatDiv.style.width = '500px';
+            webchatDiv.style.height = '500px';
+            // Z-index très élevé pour être sûr qu'il passe devant tout le reste du site
+            webchatDiv.style.zIndex = '2147483647'; 
+            document.body.appendChild(webchatDiv);
+        }
+
+        // 4. Initialiser Botpress
+        botpressScript.onload = () => {
+            window.botpress.on("webchat:ready", () => {
+                window.botpress.open();
+            });
+
+window.botpress.init({
+                botId: "a5fde69b-bdad-4b1d-932b-29f7ede999b8",
+                configuration: {
+                    version: "v2",
+                    composerPlaceholder: "Posez votre question...",
+                    botName: "A Stretta ★★★",
+                    botAvatar: "https://files.bpcontent.cloud/2026/04/16/18/20260416181213-E05HQC5E.png",
+                    botDescription: "Plongez dans l’élégance de nos chambres et suites conçues pour vous offrir le luxe moderne tout en capturant l’essence de la beauté méditerranéenne, créant ainsi un cadre parfait pour votre séjour.",
+					fabImage: "https://files.bpcontent.cloud/2026/04/16/18/20260416181236-HWVRDR7V.jpeg",
+                    website: { title: "Site Web", link: "https://hotel-astretta.com/" },
+                    email: { title: "contact@hotel-astretta.com", link: "contact@hotel-astretta.com" },
+                    phone: { title: "+33618455203", link: "+33618455203" },
+                    termsOfService: {},
+                    privacyPolicy: {},
+                    color: "#f26b41",
+                    variant: "solid",
+                    additionalStylesheetUrl: "https://kenobi-project.github.io/assistant/hotel_astretta-website_style.css",
+                    headerVariant: "glass",
+                    themeMode: "light",
+                    fontFamily: "Inter",
+                    radius: 2,
+                    feedbackEnabled: false,
+                    footer: "[Développé par MUVIA. 🚀](https://muvia-solutions.fr/)",
+                    storageLocation: "localStorage",
+                    soundEnabled: true,
+                    proactiveMessageEnabled: true,
+                    proactiveBubbleMessage: "Besoin d'aide ?👋",
+                    proactiveBubbleTriggerType: "afterDelay",
+                    proactiveBubbleDelayTime: 5
+                },
+                clientId: "ff8e1ae8-384d-4a4c-8dc0-9fc0923f142c",
+                selector: "#webchat"
+            });
+
+             window.botpress.on("webchat:ready", () => {
+    // On vérifie dans le stockage local si le bot a déjà été déclenché
+    const hasTriggeredBefore = localStorage.getItem("botpress_permanent_trigger");
+    if (!hasTriggeredBefore) {
+      window.botpress.sendEvent({type:"siteweb"});
+      // On enregistre l'information de manière permanente
+      localStorage.setItem("botpress_permanent_trigger", "true");
+    }
+  });
+            
+        };
+    }
+
+    // Lancement intelligent (Dès que possible)
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        initBotpress();
+    } else {
+        document.addEventListener("DOMContentLoaded", initBotpress);
+    }
+})();
